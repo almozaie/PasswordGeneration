815998d2dcebcc19cbb705180ec930b35164b820
almozaie <saleh.alhashemi@gmail.com>
2026-03-09 14:15:58 +0300

    Update index.js coment
    for review the functions

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function passwordBox() {
 
 const clipboardSVG = `<svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect><path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path></svg>`;
 const checkSVG     = `<svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="20 6 9 17 4 12"></polyline></svg>`;
-
+//review the function here
 function copyPassword(spanId, btn) {
   const text = document.getElementById(spanId).textContent;
   if (!text) return;
